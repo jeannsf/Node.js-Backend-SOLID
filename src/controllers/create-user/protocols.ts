69a1d711f49646a99a4266e07325cba35dc4d51c
Jean Freitas <jeannsfreitas@gmail.com>
2024-02-10 13:23:26 -0300

    feat: add MongoCreateUserRepository

---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,13 @@
+import { IUser } from "../../models/user";
+
+export interface CreateUserParams{
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface ICreateUserRepository{
+    createUser(params: CreateUserParams): Promise<IUser>;
+
+}
