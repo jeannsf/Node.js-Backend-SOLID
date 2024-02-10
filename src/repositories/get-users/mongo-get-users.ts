074e972b82c66e98a132f496650828866bb59c51
Jean Freitas <jeannsfreitas@gmail.com>
2024-02-10 12:11:03 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,8 +1,13 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { IUser} from "../../models/user"
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
     async getUsers(): Promise<IUser[]> {
+        const users = await MongoClient.db.collection<User>('users').find({}).toArray;
+        users[0]._
+        
+        
         return [
             {
             firstName: 'Jean',
